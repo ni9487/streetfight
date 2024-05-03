6435511154624e2bc8f5877da2e996f73ce08ab8
huikkh <144972281+huikkh@users.noreply.github.com>
2024-05-03 20:59:08 +0800

    Update playerexplode.cs

--- Assets/scripts/playerexplode.cs
+++ Assets/scripts/playerexplode.cs
@@ -7,7 +7,7 @@ public class playerexplode : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Destroy(this.gameObject, 1f);
+        Destroy(this.gameObject, 0.2f);
     }
 
     // Update is called once per frame
