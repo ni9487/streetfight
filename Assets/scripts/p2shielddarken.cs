611d3424626e4ffed1d8bbef173cfef3807c31d2
ni9487 <ni930927@gmail.com>
2024-05-20 19:33:34 +0800

    格黨冷卻圖

---
+++ Assets/scripts/p2shielddarken.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class p2shielddarken : MonoBehaviour
+{
+    public Image targetImage; // 要變暗的圖片
+
+    void Update()
+    {
+        // 按下數字鍵1時觸發
+        if (Input.GetKeyDown(KeyCode.DownArrow)&&targetImage.color == Color.white)
+        {
+            StartCoroutine(DarkenImageCoroutine());
+        }
+    }
+
+    IEnumerator DarkenImageCoroutine()
+    {
+        // 將圖片顏色變暗
+        targetImage.color = new Color32( 100, 100, 100, 200 );
+
+        // 等待三秒
+        yield return new WaitForSeconds(5f);
+
+        // 恢復圖片原始顏色
+        targetImage.color = Color.white;
+    }
+}
+
