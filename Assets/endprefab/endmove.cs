dbcda80831246c0b9c00726e24a16606af3730f2
ni9487 <ni930927@gmail.com>
2024-05-29 00:59:31 +0800

    勝利場警

---
+++ Assets/endprefab/endmove.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class endmove : MonoBehaviour
+{
+    public float speed = 5f;       // 移动速度
+    public float jumpForce = 10f;      // 跳跃力度
+    private Rigidbody2D rb;
+    private SpriteRenderer sprite;
+    private int extrajump = 2;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        sprite = GetComponent<SpriteRenderer>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            rb.velocity =new Vector2(-speed,rb.velocity.y);
+            //transform.Translate(-speed * Time.deltaTime, 0, 0);
+            float curspeed = -speed * Time.deltaTime;
+            flip(curspeed);
+            //anim.SetBool("jump",false);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            rb.velocity =new Vector2(speed,rb.velocity.y);
+            //transform.Translate(speed * Time.deltaTime, 0, 0);
+            float curspeed = speed * Time.deltaTime;
+            flip(curspeed);
+            //anim.SetBool("jump",false);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (extrajump > 0)
+            {
+                rb.velocity =new Vector2(rb.velocity.x,jumpForce);
+                //rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+                extrajump -= 1;
+            }
+            //anim.SetBool("jump",true);
+        }
+    }
+
+    void flip(float dirx)
+    {
+        if (dirx > 0f)
+        {
+            sprite.flipX = false;
+        }
+        else if (dirx < 0f)
+        {
+            sprite.flipX = true;
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        if (coll.gameObject.tag == "ground")
+        {
+            extrajump = 2;
+        }
+
+    }
+}
--- Assets/scripts/EndSceneManager.cs
+++ Assets/scripts/EndSceneManager.cs
@@ -3,6 +3,13 @@ using TMPro;
 
 public class EndSceneManager : MonoBehaviour
 {
+    public GameObject player1;
+    public GameObject player2;
+    public GameObject player3;
+    public GameObject player12;
+    public GameObject player21;
+    public GameObject player23;
+
     public TMP_Text resultText; // 请在 Unity 编辑器中将此文本对象链接到你的 UI 文本组件
 
     void Start()
@@ -15,10 +22,34 @@ public class EndSceneManager : MonoBehaviour
         if (firstHealth > secondHealth)
         {
             resultText.text = "Player1 Wins!";
+            if(PlayerInfo.firstCharacterSelected=="allain")
+            {
+                Instantiate(player1, new Vector3(471, 250, 0), Quaternion.identity);
+            }
+            if(PlayerInfo.firstCharacterSelected=="liliana")
+            {
+                Instantiate(player12, new Vector3(471, 250, 0), Quaternion.identity);
+            }
+            if(PlayerInfo.firstCharacterSelected=="yorn")
+            {
+                Instantiate(player3, new Vector3(471, 250, 0), Quaternion.identity);
+            }
         }
         else if (secondHealth > firstHealth)
         {
             resultText.text = "Player2 Wins!";
+            if(PlayerInfo.secondCharacterSelected=="allain2")
+            {
+                Instantiate(player21, new Vector3(471, 250, 0), Quaternion.identity);
+            }
+            if(PlayerInfo.secondCharacterSelected=="liliana2")
+            {
+                Instantiate(player2, new Vector3(471, 250, 0), Quaternion.identity);
+            }
+            if(PlayerInfo.secondCharacterSelected=="yorn2")
+            {
+                Instantiate(player23, new Vector3(471, 250, 0), Quaternion.identity);
+            }
         }
         else
         {
